d5da81ce0aabeac57d9589c0d41bd0091ec0e93f
ricardo16 <ricardopernia22@gmail.com>
2023-05-09 10:43:15 -0400

    barra roja de anuncios

---
+++ src/components/Atoms/announcement-bar/Announcement-bar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const AnnouncementBar = ()=> {
+    return <>
+        <div>
+            <div className="bg-red-600 text-white flex items-center justify-center">
+                <p className="uppercase text-xs tracking-widest text-center px-4 py-3">
+                    ANUNCIOOOS DE PROMO PRODUCTOS EN DESCUENTO!!!!!
+                </p>
+            </div>
+        </div>
+
+    </>
+}
+
+export default AnnouncementBar
